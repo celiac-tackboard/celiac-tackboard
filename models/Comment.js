64daf4015e26f5f07bef0f737a3cf20abf93c3bb
Shay Wagner <srwagner916@gmail.com>
2021-06-02 19:20:17 -0500

    wrote comment model

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Comment extends Model {};
+
+Comment.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  comment_text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'post',
+      key: 'id'
+    }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: false,
+  underscored: true,
+  modelName: 'comment'
+});
+
+module.exports = Comment;
