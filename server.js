bf2b22792e9ea1d3e75755e59aaea313fb73de93
dcheever <cheeverdanny@gmail.com>
2021-06-03 15:47:42 -0500

    handlebars running

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const Handlebars = require('handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
