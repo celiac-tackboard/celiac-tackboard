e432550efa57ab045286fa8afcfddd483e13f1d9
dcheever <cheeverdanny@gmail.com>
2021-06-04 17:24:03 -0500

    add login.handlebars and login.js

---
+++ public/javascript/login.js
@@ -0,0 +1,56 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector("#email-login").nodeValue.trim();
+    const password = document.querySelector("#password-login").value.trim();
+
+    if (username && password) {
+        // is this route correct?:
+        const response = await fetch("/api/users/login", {
+            method: "post",
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        if (response.ok) {
+            // im sending logged in user to homepage, not sure if that is correct
+            document.location.replace("/homepage");
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector("#username-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if (username && email && password) {
+        // is this route correct?:
+        const response = await fetch("/api/users", {
+            method: "post",
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        if (response.ok) {
+            console.log("success");
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
+
+document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
