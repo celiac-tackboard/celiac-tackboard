05bc7428cad7dbc69862850bf962f814e3347a2e
Shay Wagner <srwagner916@gmail.com>
2021-06-04 00:10:45 -0500

    wrote routes to get one post by ID, update a post by ID, and delete a post by ID

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
-const { Post, User, Votes, Location, Comment } = require('../../models');
+const { Post } = require('../../models');
 
 // GET all posts
 router.get('/', (req, res) => {
   Post.findAll({
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
+    // include: [
+    //   {
+    //     model: Comment,
+    //     attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //     include: {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   },
+    //   {
+    //     model: User,
+    //     attributes: ['username']
+    //   }
+    // ]
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -25,9 +25,30 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+//==================================================
+// GET one post by ID
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "no post found with this ID" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+//===================================================
 
 // CREATE a post
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
   Post.create({
     title: req.body.title,
     description: req.body.description,
@@ -41,5 +62,52 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+//=================================================
+
+// UPDATE a post by ID
+router.put('/:id', (req, res) => {
+  Post.update({
+    title: req.body.title,
+    description: req.body.description,
+    post_url: req.body.post_url
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(500).json({ message: "no post found with this ID" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+//=====================================================
+
+// DELETE a post by ID
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "no post found with this ID" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+//======================================================
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -42,6 +42,10 @@ Comment.belongsTo(Post, {
   foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 User.hasMany(Comment, {
   foreignKey: 'user_id'
 });
