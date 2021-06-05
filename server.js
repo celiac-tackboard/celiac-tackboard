a307b8a31a8044f935fa02c397e67fd87436895a
Vasilyg10 <zvgianna@gmail.com>
2021-06-04 22:44:51 -0500

    added helpers

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const { ppid } = require('process');
 const routes = require('./controllers');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
