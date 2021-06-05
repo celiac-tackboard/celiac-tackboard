09c5b3e9f87f77c061f96d63e5ac573bccea1c19
Vasilyg10 <zvgianna@gmail.com>
2021-06-04 22:09:56 -0500

    added express-session things to server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const { ppid } = require('process');
 const routes = require('./controllers');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitiated: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 app.use(routes);
 
