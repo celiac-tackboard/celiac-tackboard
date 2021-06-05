6aa6fa4d4946aca3f106d921838dddcf5381157d
Shay Wagner <srwagner916@gmail.com>
2021-06-05 15:38:56 -0500

    added 'rating' field to the Post model

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
       'title',
       'description',
       'post_url',
+      'rating',
       'user_id',
       'location_id',
       'createdAt',
@@ -50,6 +51,7 @@ router.get('/:id', (req, res) => {
         'title',
         'description',
         'post_url',
+        'rating',
         'user_id',
         'location_id',
         'createdAt',
@@ -92,6 +94,7 @@ router.post('/', (req, res) => {
     title: req.body.title,
     description: req.body.description,
     post_url: req.body.post_url,
+    rating: req.body.rating,
     user_id: req.body.user_id,
     location_id: req.body.location_id
   })
--- models/Post.js
+++ models/Post.js
@@ -24,6 +24,14 @@ Post.init({
       isUrl: true
     }
   },
+  rating: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    validate: {
+      min: 0,
+      max: 5
+    }
+  },
   user_id: {
     type: DataTypes.INTEGER,
     allowNull: false,
