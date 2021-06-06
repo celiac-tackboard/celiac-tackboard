a5c145927df32314a14f192639569e51355d30e9
dcheever <cheeverdanny@gmail.com>
2021-06-06 15:01:41 -0500

    add upvote.js file

---
+++ public/javascript/upvote.js
@@ -0,0 +1,27 @@
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+
+    // make sure these routes are correct
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        bod: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+// make sure this id is correct
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
