4041c043f2051543aa56b8ab1a702d511259e38f
allan.pirillis <allan.p94@gmail.com>
2021-06-07 16:18:55 -0500

    integraded the sign-up/login html with the tile structure

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,7 +5,20 @@ router.get("/", (req, res) => {
 });
 
 router.get("/posts", (req, res) => {
+  //need to go through and query all the relevant posts and pass them through to the handlebar
+  //
+  //
+  //
   res.render("posts");
+
+  //once the query is set up and returns the data we will call
+  //res.render("posts", {
+  // data,
+  // loggedIn: req.session.loggedIn})
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
