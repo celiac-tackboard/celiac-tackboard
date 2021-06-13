c0b53faabfc4caa526d6f90c321fb270ed600273
Shay Wagner <srwagner916@gmail.com>
2021-06-13 00:50:53 -0500

    profile-page.handlebars displays user's posts; profile-page-comments.handlebars is displaying comments

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,14 @@ const apiRoutes = require('./api')
 const homeRoutes = require('./home-routes');
 const profileRoutes = require('./profile-routes');
 const submitRoutes = require('./submit-routes');
+const profilePageRoutes = require('./profile-page-routes');
 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/profile', profileRoutes);
-router.use('/submit', submitRoutes)
+router.use('/submit', submitRoutes);
+router.use('/profile-page', profilePageRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
