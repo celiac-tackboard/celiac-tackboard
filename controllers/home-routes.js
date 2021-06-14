6f24adef97659843cffc3aa53a336528a4bac1fe
Shay Wagner <srwagner916@gmail.com>
2021-06-14 18:58:15 -0500

    added console.log on line 68

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -64,6 +64,7 @@ router.get("/", (req, res) => {
               data.push(location.get({ plain: true }));
             });
             data.push(...posts);
+            console.log(data);
         });
         res.render("homepage", {
           data, 
