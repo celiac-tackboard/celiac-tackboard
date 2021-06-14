6a2b5ff30ced55bf246dbf58476689585b8a17b1
Shay Wagner <srwagner916@gmail.com>
2021-06-13 21:12:56 -0500

    added get location route

--- controllers/api/location-routes.js
+++ controllers/api/location-routes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const { Location } = require('../../models');
 const sequelize = require('../../config/connection');
+const { RSA_NO_PADDING } = require('constants');
+
+router.get('/', (req, res) => {
+  Location.findAll({})
+    .then(dbLocationData => {res.json(dbLocationData)})
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.post("/", (req, res) => {
     Location.create({
